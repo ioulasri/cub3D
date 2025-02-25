155081f5f02c73155858d7e3dc343d1d0a00054b
imoulasr <imoulasr@c3r6p8.1337.ma>
2025-02-25 16:43:29 +0000

    created a 2D version

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,43 +6,46 @@
 /*   By: imoulasr <imoulasr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 10:58:38 by imoulasr          #+#    #+#             */
-/*   Updated: 2025/02/25 16:25:06 by imoulasr         ###   ########.fr       */
+/*   Updated: 2025/02/25 16:43:27 by imoulasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct s_map_file {
-    char **arr;
-    int size;
-    int capacity;
-} t_map_file;
+#ifndef CUB3D_H
+# define CUB3D_H
 
-typedef struct s_textures {
-    char	*no_texture;
-    char	*so_texture;
-    char	*we_texture;
-    char	*ea_texture;
-} t_textures;
+# define CELL_SIZE 20
+# define ESCKEY 65307
 
-typedef struct s_colors {
-    int ceiling_color;
-    int floor_color;
-} t_colors;
-
-typedef struct s_map {
-    char **grid;
-    int width;
-    int height;
-    int start_x;
-    int start_y;
-    char start_dir; // 'N', 'S', 'E', 'W'
-} t_map;
-
-typedef struct s_player
+typedef struct s_textures
 {
-	double	x;     /* Pixel coordinate */
-	double	y;     /* Pixel coordinate */
-	double	angle; /* Direction in radians (optional) */
-}				t_player;
+	char	*no_texture;
+	char	*so_texture;
+	char	*we_texture;
+	char	*ea_texture;
+}				t_textures;
+
+typedef struct s_colors
+{
+	int	floor_color;
+	int	ceiling_color;
+}				t_colors;
+
+typedef struct s_map
+{
+	char	**grid;
+	int		width;
+	int		height;
+	int		start_x;
+	int		start_y;
+	char	start_dir;
+}				t_map;
+
+typedef struct s_map_file
+{
+	char	**arr;
+	int		size;
+	int		capacity;
+}				t_map_file;
 
 typedef struct s_keys
 {
@@ -52,21 +55,43 @@ typedef struct s_keys
 	int	right;
 }				t_keys;
 
-typedef struct s_config
+typedef struct s_player
+{
+	double	x;
+	double	y;
+	double	angle;
+}				t_player;
+
+typedef struct s_graphics
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*img_data;
+	int		bpp;
+	int		line_length;
+	int		endian;
+}				t_graphics;
+
+typedef struct s_assets
 {
 	t_textures	*textures;
 	t_colors	*colors;
+}				t_assets;
+
+typedef struct s_game
+{
 	t_map		*map;
 	t_map_file	*file;
-    t_keys      keys;
-    t_player	*player;
-	void		*mlx;
-	void		*win;
-    void		*img;
-	char		*img_data;
-	int			bpp;
-	int			line_length;
-	int			endian;
+	t_keys		keys;
+	t_player	*player;
+}				t_game;
+
+typedef struct s_config
+{
+	t_assets	*assets;
+	t_game		*game;
+	t_graphics	*graphics;
 }				t_config;
 
 typedef enum e_phase
@@ -100,4 +125,3 @@ void    create_game_map(t_map_file *file, t_config *config);
 void	clean_and_exit(t_config *config, t_map_file *file);
 void print_map(const t_map *map);
 int	is_empty_line(const char *line);
-void	draw_player(t_config *config);
